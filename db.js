760ae29ce85925cde8eddcc372cbace51caaf7b7
Sherezz Grant <rezziemaven@gmail.com>
2018-08-13 15:48:20 +0200

    initial commit

---
+++ db.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
+});
+
+connection.connect(err => {
+  if (err) throw err;
+  console.log('Connected to database.');
+});
+
+module.exports = connection;
---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const logger = require('koa-logger');
+const Koa = require('koa');
+const app = new Koa();
+const cors = require('koa-cors');
+const bodyParser = require('koa-bodyparser');
+const router = require('./router');
+
+app
+  .use(logger())
+  .use(cors())
+  .use(bodyParser())
+  // .use(router.routes());
+
+ const port = process.env.PORT || 3000;
+
+ app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+ })
