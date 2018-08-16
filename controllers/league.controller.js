5b5fb74d2e2f820f1d3ede0f11812f80d9a176e3
Sherezz Grant <rezziemaven@gmail.com>
2018-08-16 11:18:14 +0200

    fix: use reduce to transform data sent to league endpoints

--- controllers/league.controller.js
+++ controllers/league.controller.js
@@ -4,7 +4,27 @@ const leagueModel = require('../models/league.model');
 
 exports._getLeagues = async (ctx) => {
   try {
-    ctx.body = await leagueModel._getAll(ctx.params.cityName);
+    const leagues = await leagueModel._getAll(ctx.params.cityName);
+    const collection = await leagues.reduce((acc,league) => {
+      const {league_id, sport_id, name, ...rest} = league;
+      if (!acc.hasOwnProperty(league.league_id)) acc[league.league_id] = [{
+        league_id,
+        sport_id,
+        sport_name: name,
+      }, rest];
+      else acc[league.league_id].push(rest);
+      return acc;
+    },{});
+      ctx.body = await Object.values(collection).reduce((acc, entry) => {
+      const [leagueData, ...rest] = entry;
+      acc.push({
+        league_id: leagueData.league_id,
+        sport_id: leagueData.sport_id,
+        sport_name: leagueData.name,
+        users: rest
+      })
+      return acc;
+    },[]);
     ctx.status = 200;
   }
   catch (e) {
@@ -15,7 +35,17 @@ exports._getLeagues = async (ctx) => {
 
 exports._getLeague = async (ctx) => {
   try {
-    ctx.body = await leagueModel._getOne(ctx.params.leagueId);
+    const league = await leagueModel._getOne(ctx.params.leagueId);
+    ctx.body = await league.reduce((acc, entry) => {
+      const {league_id, sport_id, name, ...rest} = entry;
+      acc.users.push(rest);
+      return acc;
+    },{
+      league_id: league[0].league_id,
+      sport_id: league[0].sport_id,
+      sport_name: league[0].name,
+      users:[]
+    });
     ctx.status = 200;
   }
   catch (e) {
