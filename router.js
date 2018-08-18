4efb10d9e954e7f0082208816cd1aac93da85b3e
Sherezz Grant <rezziemaven@gmail.com>
2018-08-18 17:55:07 +0200

    fix: add set match route, update finish match route

--- router.js
+++ router.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const Router = require('koa-router');
 const router = new Router();
 
@@ -5,13 +7,15 @@ const userController = require('./controllers/user.controller');
 const leagueController = require('./controllers/league.controller');
 const matchController = require('./controllers/match.controller');
 const ulController = require('./controllers/users-leagues.controller');
+const statsController = require('./controllers/stats.controller');
 
 router
   // .get('/users/:id', userController.getUser)
   .get('/:cityName/leagues', leagueController.getLeagues)
   .get('/:cityName/leagues/:leagueId', leagueController.getLeague)
   .get('/users/:userId/:cityName/matches', matchController.getMatches)
+  .post('/matches/:matchId/set', matchController.setDetails, matchController.getMatch)
   .put('/matches/:matchId/:action', matchController.changeStatus, matchController.getMatch)
-  .post('/matches/:matchId/:action', matchController.changeStatus, ulController.setNewElo, matchController.finishMatch, matchController.getMatch);
+  .post('/matches/:matchId/:action', matchController.changeStatus, ulController.setNewElo, statsController.updateStats, matchController.finishMatch, matchController.getMatch);
 
 module.exports = router;
