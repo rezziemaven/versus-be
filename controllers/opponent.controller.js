510ad158437c3134568650bad545ad1435eca942
Stefano Russo <stefanorusso1@ied.edu>
2018-08-20 23:23:57 +0200

    fix: opponent controller

--- controllers/opponent.controller.js
+++ controllers/opponent.controller.js
@@ -11,13 +11,14 @@ exports.getOpponent = async (ctx) => {
 
         accum.matches_history = accum.matches_history.concat({
           score: `${el.user1_score} - ${el.user2_score}`,
+
         })
 
         return accum
       },{
         user_id:data[0].user_id,
         username:data[0].username,
-        score:data[0].elo_rating,
+        score:data[0].current_elo,
         image_path:data[0].user_image_path,
         matches_won:data[0].matches_won,
         matches_lost:data[0].matches_lost,
