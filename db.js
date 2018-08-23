0f767ca07e68324544cf8f85fdc12c52e5a2806e
Sherezz 'Rezzie' Grant <rezziemaven@gmail.com>
2018-08-23 10:18:31 +0200

    Update db.js
    Fix: Remove insecure auth line

--- db.js
+++ db.js
@@ -6,7 +6,6 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  insecureAuth: true
 });
 
 connection.connect(err => {
